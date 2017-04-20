6d32e6568f8be109d32edcad49f8539dcddaab05
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-04-20 12:29:24 -0700

    add WebsocketProxy.Copy field (highly sourcegraph-specific)

--- websocketproxy.go
+++ websocketproxy.go
@@ -2,7 +2,7 @@
 package websocketproxy
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -44,6 +44,10 @@ type WebsocketProxy struct {
 	//  Dialer contains options for connecting to the backend WebSocket server.
 	//  If nil, DefaultDialer is used.
 	Dialer *websocket.Dialer
+
+	// Copy is invoked to perform the bi-direction copy of data between the
+	// public client and proxy backend.
+	Copy func(client *websocket.Conn, backend func() (*websocket.Conn, error)) error
 }
 
 // ProxyHandler returns a new http.Handler interface that reverse proxies the
@@ -76,103 +80,83 @@ func (w *WebsocketProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	backendURL := w.Backend(req)
-	if backendURL == nil {
-		log.Println("websocketproxy: backend URL is nil")
-		http.Error(rw, "internal server error (code: 2)", http.StatusInternalServerError)
-		return
-	}
-
-	dialer := w.Dialer
-	if w.Dialer == nil {
-		dialer = DefaultDialer
-	}
-
-	// Pass headers from the incoming request to the dialer to forward them to
-	// the final destinations.
-	requestHeader := http.Header{}
-	if origin := req.Header.Get("Origin"); origin != "" {
-		requestHeader.Add("Origin", origin)
-	}
-	for _, prot := range req.Header[http.CanonicalHeaderKey("Sec-WebSocket-Protocol")] {
-		requestHeader.Add("Sec-WebSocket-Protocol", prot)
-	}
-	for _, cookie := range req.Header[http.CanonicalHeaderKey("Cookie")] {
-		requestHeader.Add("Cookie", cookie)
-	}
-
-	// Pass X-Forwarded-For headers too, code below is a part of
-	// httputil.ReverseProxy. See http://en.wikipedia.org/wiki/X-Forwarded-For
-	// for more information
-	// TODO: use RFC7239 http://tools.ietf.org/html/rfc7239
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		// If we aren't the first proxy retain prior
-		// X-Forwarded-For information as a comma+space
-		// separated list and fold multiple headers into one.
-		if prior, ok := req.Header["X-Forwarded-For"]; ok {
-			clientIP = strings.Join(prior, ", ") + ", " + clientIP
-		}
-		requestHeader.Set("X-Forwarded-For", clientIP)
-	}
-
-	// Set the originating protocol of the incoming HTTP request. The SSL might
-	// be terminated on our site and because we doing proxy adding this would
-	// be helpful for applications on the backend.
-	requestHeader.Set("X-Forwarded-Proto", "http")
-	if req.TLS != nil {
-		requestHeader.Set("X-Forwarded-Proto", "https")
-	}
-
-	// Enable the director to copy any additional headers it desires for
-	// forwarding to the remote server.
-	if w.Director != nil {
-		w.Director(req, requestHeader)
-	}
-
-	// Connect to the backend URL, also pass the headers we get from the requst
-	// together with the Forwarded headers we prepared above.
-	// TODO: support multiplexing on the same backend connection instead of
-	// opening a new TCP connection time for each request. This should be
-	// optional:
-	// http://tools.ietf.org/html/draft-ietf-hybi-websocket-multiplexing-01
-	connBackend, resp, err := dialer.Dial(backendURL.String(), requestHeader)
-	if err != nil {
-		log.Printf("websocketproxy: couldn't dial to remote backend url %s\n", err)
-		return
-	}
-	defer connBackend.Close()
-
 	upgrader := w.Upgrader
 	if w.Upgrader == nil {
 		upgrader = DefaultUpgrader
 	}
 
-	// Only pass those headers to the upgrader.
-	upgradeHeader := http.Header{}
-	if hdr := resp.Header.Get("Sec-Websocket-Protocol"); hdr != "" {
-		upgradeHeader.Set("Sec-Websocket-Protocol", hdr)
-	}
-	if hdr := resp.Header.Get("Set-Cookie"); hdr != "" {
-		upgradeHeader.Set("Set-Cookie", hdr)
-	}
-
 	// Now upgrade the existing incoming request to a WebSocket connection.
-	// Also pass the header that we gathered from the Dial handshake.
-	connPub, err := upgrader.Upgrade(rw, req, upgradeHeader)
+	connPub, err := upgrader.Upgrade(rw, req, nil)
 	if err != nil {
 		log.Printf("websocketproxy: couldn't upgrade %s\n", err)
 		return
 	}
 	defer connPub.Close()
 
-	errc := make(chan error, 2)
-	cp := func(dst io.Writer, src io.Reader) {
-		_, err := io.Copy(dst, src)
-		errc <- err
-	}
-
 	// Start our proxy now, everything is ready...
-	go cp(connBackend.UnderlyingConn(), connPub.UnderlyingConn())
-	go cp(connPub.UnderlyingConn(), connBackend.UnderlyingConn())
+	errc := make(chan error, 1)
+	go func() {
+		err := w.Copy(connPub, func() (*websocket.Conn, error) {
+			backendURL := w.Backend(req)
+			if backendURL == nil {
+				return nil, fmt.Errorf("websocketproxy: backend URL is nil")
+			}
+
+			dialer := w.Dialer
+			if w.Dialer == nil {
+				dialer = DefaultDialer
+			}
+
+			// Pass headers from the incoming request to the dialer to forward them to
+			// the final destinations.
+			requestHeader := http.Header{}
+			if origin := req.Header.Get("Origin"); origin != "" {
+				requestHeader.Add("Origin", origin)
+			}
+			for _, prot := range req.Header[http.CanonicalHeaderKey("Sec-WebSocket-Protocol")] {
+				requestHeader.Add("Sec-WebSocket-Protocol", prot)
+			}
+			for _, cookie := range req.Header[http.CanonicalHeaderKey("Cookie")] {
+				requestHeader.Add("Cookie", cookie)
+			}
+
+			// Pass X-Forwarded-For headers too, code below is a part of
+			// httputil.ReverseProxy. See http://en.wikipedia.org/wiki/X-Forwarded-For
+			// for more information
+			// TODO: use RFC7239 http://tools.ietf.org/html/rfc7239
+			if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+				// If we aren't the first proxy retain prior
+				// X-Forwarded-For information as a comma+space
+				// separated list and fold multiple headers into one.
+				if prior, ok := req.Header["X-Forwarded-For"]; ok {
+					clientIP = strings.Join(prior, ", ") + ", " + clientIP
+				}
+				requestHeader.Set("X-Forwarded-For", clientIP)
+			}
+
+			// Set the originating protocol of the incoming HTTP request. The SSL might
+			// be terminated on our site and because we doing proxy adding this would
+			// be helpful for applications on the backend.
+			requestHeader.Set("X-Forwarded-Proto", "http")
+			if req.TLS != nil {
+				requestHeader.Set("X-Forwarded-Proto", "https")
+			}
+
+			// Enable the director to copy any additional headers it desires for
+			// forwarding to the remote server.
+			if w.Director != nil {
+				w.Director(req, requestHeader)
+			}
+
+			// Connect to the backend URL, also pass the headers we get from the requst
+			// together with the Forwarded headers we prepared above.
+			connBackend, _, err := dialer.Dial(backendURL.String(), requestHeader)
+			if err != nil {
+				return nil, fmt.Errorf("websocketproxy: couldn't dial to remote backend url %s\n", err)
+			}
+			return connBackend, nil
+		})
+		errc <- err
+	}()
 	<-errc
 }
